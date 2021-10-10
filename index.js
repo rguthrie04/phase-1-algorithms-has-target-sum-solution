b8723551bf24a9fa592264554109471e9f59d4b8
Ryan Guthrie <ryanguthrie@zoho.com>
2021-10-10 22:50:46 +0100

    commit tree 7cc1c915785b443616f96cb5f7d9b6121b49553b

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    if (seenNumbers[complement]) return true
+    seenNumbers[array[i]] = true
+  }
+  return false
 }
+
+
+
+// function hasTargetSum(array, target) {
+//   for( let i = 0; i< array.length; i++) {
+//     // n steps
+    
+//     const complement = target - array[i]
+//     for (let j = i + 1; j< array.length; j++){
+//       //n *n steps
+//       if (array[j] === complement) return true
+// }
+//   }
+//   return false
+// }
 
 /* 
   Write the Big O time complexity of your function here
+Runtime: O(n squared)
+Space Complexity: o(n)
+
 */
 
 /* 
